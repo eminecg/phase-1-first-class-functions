bacfd5ca0fd84f291d6f34368381335aba532485
Emine Çığ <emine.cig67@gmail.com>
2022-04-26 16:25:46 +0300

    Assignment Completed

---
+++ index.js
@@ -0,0 +1,20 @@
+// code
+
+
+function receivesAFunction(callback ){
+    callback();
+
+
+}
+
+function returnsANamedFunction(){
+    return function named(){
+       
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+
+    }
+}
